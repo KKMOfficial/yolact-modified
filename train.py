05105d911dfbdba0b261fc4d7794266d8d19115a
KKMOfficial <89024919+KKMOfficial@users.noreply.github.com>
2024-07-27 16:09:12 +0330

    Update train.py

--- train.py
+++ train.py
@@ -517,7 +517,8 @@ def train():
             # This is done per epoch
             if args.validation_epoch > 0:
                 if epoch % args.validation_epoch == 0 and epoch > 0:
-                    compute_validation_map(epoch, iteration, yolact_net, val_dataset, log if args.log else None)
+                    compute_validation_map(epoch, iteration, yolact_net, dataset, log if args.log else None)
+                    
         
         # Compute validation mAP after training is finished
         compute_validation_map(epoch, iteration, yolact_net, val_dataset, log if args.log else None)
